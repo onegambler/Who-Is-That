280ae27220ac4c55449e5f3ddfac6e4510df27db
Roberto Magale <roberto.magale@gmail.com>
2015-07-24 16:50:55 +0100

    Added english grammar

--- src/main/java/com/whoisthat/grammars/ItalianQuestionListener.java
+++ src/main/java/com/whoisthat/grammars/ItalianQuestionListener.java
@@ -4,20 +4,20 @@ import com.whoisthat.GameTerminal;
 import com.whoisthat.grammars.italian.ItalianBaseListener;
 import com.whoisthat.grammars.italian.ItalianParser;
 import com.whoisthat.system.question.Question;
+import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 import static com.whoisthat.grammars.italian.ItalianLexer.*;
 import static com.whoisthat.grammars.italian.ItalianTerminal.getTerminal;
 
-public class ItalianQuestionListener extends ItalianBaseListener {
+public class ItalianQuestionListener extends ItalianBaseListener implements QuestionListener {
 
     private Question.QuestionBuilder questionBuilder;
 
     @Override
     public void enterGame(ItalianParser.GameContext ctx) {
-        questionBuilder = Question.builder()
-                .withPronoun(GameTerminal.MAN);
+        questionBuilder = Question.builder();
     }
 
     public Question getQuestion() {
@@ -26,6 +26,9 @@ public class ItalianQuestionListener extends ItalianBaseListener {
 
     @Override
     public void enterQuestion(ItalianParser.QuestionContext ctx) {
+
+        questionBuilder.withPronoun(GameTerminal.MAN);
+
         TerminalNode child = ctx.getChild(TerminalNode.class, 0);
         switch (child.getSymbol().getType()) {
             case HAVE:
@@ -44,14 +47,38 @@ public class ItalianQuestionListener extends ItalianBaseListener {
     @Override
     public void enterLook(ItalianParser.LookContext ctx) {
         ParseTree lookNode = ctx.getChild(0);
-        if(lookNode.getChildCount() > 0) {
+        if(hasOnlyPhysicalTrait(lookNode)) {
+            questionBuilder.withPhysicalTrait(getTerminal(ctx.getText()));
+        } else {
             questionBuilder.withPhysicalTrait(getTerminal(lookNode.getChild(0).getText()));
-            if (lookNode.getChildCount() == 2) {
+            if (hasLookDefinition(lookNode)) {
                 questionBuilder.withLook(getTerminal(lookNode.getChild(1).getText()));
             }
-        } else {
-            questionBuilder.withPhysicalTrait(getTerminal(ctx.getText()));
         }
+    }
+
+    private boolean hasOnlyPhysicalTrait(ParseTree lookNode) {
+        return isTerminalNode(lookNode);
+    }
+
+    private boolean isTerminalNode(ParseTree node) {
+        return node instanceof TerminalNode;
+    }
+
+    private boolean hasLookDefinition(ParseTree lookNode) {
+        return lookNode.getChildCount() == 2;
+    }
+
+    @Override
+    public void enterSolution(ItalianParser.SolutionContext ctx) {
+        questionBuilder.answer()
+                .withSolution(ctx.getChild(0).getText());
+    }
+
+    @Override
+    public void visitErrorNode(ErrorNode node) {
+        //TODO: handle error nodes
+
     }
 
     @Override
